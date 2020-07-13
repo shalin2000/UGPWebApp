2a657071a73da3ee4bcf9d4a4c08b432d6fecaa1
Ansar Ahmed Khan <mohammedkhan5040@gmail.com>
2020-07-13 17:09:23 -0500

    cleanups completed for backend files

--- UGPWebApp/src/main/java/com/example/demo/controller/ApplicationController.java
+++ UGPWebApp/src/main/java/com/example/demo/controller/ApplicationController.java
@@ -2,35 +2,22 @@ package com.example.demo.controller;
 
 import java.util.List;
 import javax.annotation.Resource;
-
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping("/postgressApp")
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:3000") //merge of two ports
 public class ApplicationController {
     @Resource
     UserService userService;
-    @GetMapping(value = "/userList")
+    @GetMapping(value = "/userList") //getRequest
     public List<User> getUser() {
         return userService.findAll();
     }
-    @PostMapping(value = "/createUser")
+    @PostMapping(value = "/createUser") //postRequest
     public void createUser(@RequestBody User emp) {
         userService.insertUser(emp);
     }
-    // @PutMapping(value = "/updateEmp")
-    // public void updateEmployee(@RequestBody Employee emp) {
-    //     employeeService.updateEmployee(emp);
-    // }
-    // @PutMapping(value = "/executeUpdateEmp")
-    // public void executeUpdateEmployee(@RequestBody Employee emp) {
-    //     employeeService.executeUpdateEmployee(emp);
-    // }
-    // @DeleteMapping(value = "/deleteEmpById")
-    // public void deleteEmployee(@RequestBody Employee emp) {
-    //     employeeService.deleteEmployee(emp);
-    // }
 }
