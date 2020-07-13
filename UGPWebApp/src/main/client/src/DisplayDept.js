e8eebd6377f6036b446cf4d842fb80be33f3ae87
Shalin patel <shalinpatel@shalins-mbp.attlocal.net>
2020-07-13 16:33:10 -0500

    update on listDept

--- UGPWebApp/src/main/client/src/DisplayDept.js
+++ UGPWebApp/src/main/client/src/DisplayDept.js
@@ -12,119 +12,14 @@ class DisplayDept extends Component {
   }
 
   render(){
-    const someSpace = ' ... '
     return (
       <div className="App">
 
         < Header />
-        {/* <header className="Header"> <img className="logo" src={Logo} alt="logo" />UIC GradePal</header> */}
 
         <div className="secHeader"><h2>Check Your Department</h2></div>
-        <div className='Table1'>
-        {/* Using bootstrap to generate a 3 col layout and printing all the DEPT by using the ImportCsvFile class */}
-        <div class="row">
-          <div className="col-sm-4">
-            <h2>A</h2>
-            <ListDept letter = 'a'/>
-          </div>
-          
-          <div className="col-sm-4">
-            <h2>B</h2>
-            <ListDept letter = 'b'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>C</h2>
-            <ListDept letter = 'c'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>D</h2>
-            <ListDept letter = 'd'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>E</h2>
-            <ListDept letter = 'e'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>F</h2>
-            <ListDept letter = 'f'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>G</h2>
-            <ListDept letter = 'g'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>H</h2>
-            <ListDept letter = 'h'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>I</h2>
-            <ListDept letter = 'i'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>J</h2>
-            <ListDept letter = 'j'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>K</h2>
-            <ListDept letter = 'k'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>L</h2>
-            <ListDept letter = 'l'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>M</h2>
-            <ListDept letter = 'm'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>N</h2>
-            <ListDept letter = 'n'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>O</h2>
-            <ListDept letter = 'o'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>P</h2>
-            <ListDept letter = 'p'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>R</h2>
-            <ListDept letter = 'r'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>S</h2>
-            <ListDept letter = 's'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>T</h2>
-            <ListDept letter = 't'/>
-          </div>
-
-          <div className="col-sm-4">
-            <h2>U</h2>
-            <ListDept letter = 'u'/>
-          </div>
-          </div>
-        </div>
-
+        <ListDept />
+  
         <Footer />
 
       </div>
--- UGPWebApp/src/main/client/src/ListDept.js
+++ UGPWebApp/src/main/client/src/ListDept.js
@@ -48,8 +48,27 @@ class ListDept extends Component {
     // removes dup entires that have the same CRS_SUBJ_CD and store it in noDup array
     const noDupArr = this.removeDup(this.state.myData, x => x.CRS_SUBJ_CD);
 
-    // filters the noDup array by the first letter in CRS_SUBJ_CD where the first letter is passed in as props 
-    const sameLetterArr = noDupArr.filter(x => x.CRS_SUBJ_CD.toLowerCase().startsWith(this.props.letter));
+    // filters the noDup array by the first letter in CRS_SUBJ_CD 
+    const arrA = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('A'));
+    const arrB = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('B'));
+    const arrC = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('C'));
+    const arrD = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('D'));
+    const arrE = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('E'));
+    const arrF = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('F'));
+    const arrG = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('G'));
+    const arrH = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('H'));
+    const arrI = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('I'));
+    const arrJ = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('J'));
+    const arrK = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('K'));
+    const arrL = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('L'));
+    const arrM = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('M'));
+    const arrN = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('N'));
+    const arrO = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('O'));
+    const arrP = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('P'));
+    const arrR = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('R'));
+    const arrS = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('S'));
+    const arrT = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('T'));
+    const arrU = noDupArr.filter(x => x.CRS_SUBJ_CD.startsWith('U'));
 
     const columns = [{
       dataField: 'CRS_SUBJ_CD',
@@ -64,11 +83,202 @@ class ListDept extends Component {
       order: 'asc'
     }];
     
-    const displayDeptArr = <div className="react-bootstrap-table-wrapper">
+    const displayDeptArrA = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrA }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                          </div>
+
+    const displayDeptArrB = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrB }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                            </div>
+    const displayDeptArrC = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrC }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                          </div>
+    const displayDeptArrD = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrD }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                            </div>
+    const displayDeptArrE = <div className="react-bootstrap-table-wrapper">
                               <BootstrapTable
                                 bootstrap4
                                 keyField="CRS_SUBJ_CD"
-                                data={ sameLetterArr }
+                                data={ arrE }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrF = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrF }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrG = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrG }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrH = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrH }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrI = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrI }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrJ = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrJ }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrK = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrK }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                            </div>
+    const displayDeptArrL = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrL }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                            </div>
+    const displayDeptArrM = <div className="react-bootstrap-table-wrapper">
+                          <BootstrapTable
+                            bootstrap4
+                            keyField="CRS_SUBJ_CD"
+                            data={ arrM }
+                            columns={ columns } 
+                            defaultSorted={ defaultSorted } 
+                            hover
+                          />
+                          </div>
+    const displayDeptArrN = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrN }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                            </div>
+    const displayDeptArrO = <div className="react-bootstrap-table-wrapper">
+                            <BootstrapTable
+                              bootstrap4
+                              keyField="CRS_SUBJ_CD"
+                              data={ arrO }
+                              columns={ columns } 
+                              defaultSorted={ defaultSorted } 
+                              hover
+                            />
+                            </div>
+    const displayDeptArrP = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrP }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrR = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrR }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrS = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrS }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrT = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrT }
+                                columns={ columns } 
+                                defaultSorted={ defaultSorted } 
+                                hover
+                              />
+                            </div>
+    const displayDeptArrU = <div className="react-bootstrap-table-wrapper">
+                              <BootstrapTable
+                                bootstrap4
+                                keyField="CRS_SUBJ_CD"
+                                data={ arrU }
                                 columns={ columns } 
                                 defaultSorted={ defaultSorted } 
                                 hover
@@ -76,9 +286,111 @@ class ListDept extends Component {
                             </div>
 
     return (
-       <div>
-         {/* <ScrollUpButton style={{ outline:'none', backgroundColor: '#AC1E2D', borderRadius: '8px', padding: '4px',}}/> */}
-          {displayDeptArr}
+      <div>
+        <div className='Table1'>
+        {/* Using bootstrap to generate a 3 col layout and printing all the DEPT by using the ImportCsvFile class */}
+          <div class="row">
+            <div className="col-sm-4">
+              <h2>A</h2>
+              {displayDeptArrA}
+            </div>
+            
+            <div className="col-sm-4">
+              <h2>B</h2>
+              {displayDeptArrB}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>C</h2>
+              {displayDeptArrC}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>D</h2>
+              {displayDeptArrD}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>E</h2>
+              {displayDeptArrE}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>F</h2>
+              {displayDeptArrF}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>G</h2>
+              {displayDeptArrG}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>H</h2>
+              {displayDeptArrH}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>I</h2>
+              {displayDeptArrI}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>J</h2>
+              {displayDeptArrJ}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>K</h2>
+              {displayDeptArrK}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>L</h2>
+              {displayDeptArrL}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>M</h2>
+              {displayDeptArrM}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>N</h2>
+              {displayDeptArrN}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>O</h2>
+              {displayDeptArrO}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>P</h2>
+              {displayDeptArrP}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>R</h2>
+              {displayDeptArrR}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>S</h2>
+              {displayDeptArrS}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>T</h2>
+              {displayDeptArrT}
+            </div>
+
+            <div className="col-sm-4">
+              <h2>U</h2>
+              {displayDeptArrU}
+            </div>
+          </div>
+         </div>
        </div>
     );
 
