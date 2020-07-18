e49e59aaa2c01ffe6538b3cd2d8cf7fdd6b86276
Qasim Mir <mir@Qasims-MacBook-Pro.local>
2020-07-17 20:05:12 -0500

    share tab

--- UGPWebApp/src/main/client/src/Footer.js
+++ UGPWebApp/src/main/client/src/Footer.js
@@ -16,10 +16,10 @@ class Footer extends Component {
                     <p className="copyright-text">Copyright &copy; {year} All Rights Reserved by GradePal.</p>  
                     <hr/>
                     <ul className="row" style={{justifyContent: "center"}}>
-                        <li><a class="facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://www.google.com/"><i className="fab fa-facebook fa-2x"></i></a></li>
-                        <li><a class="twitter" href="https://www.twitter.com/share?url=https://www.google.com/"><i className="fab fa-twitter fa-2x"></i></a></li>
-                        <li><a class="reddit" href="http://www.reddit.com/submit?url=https://www.google.com/"><i className="fab fa-reddit-alien fa-2x"></i></a></li>
-                        <li><a class="linkedin" href="https://www.linkedin.com/sharing/share-offsite/?url=https://www.google.com/"><i className="fab fa-linkedin fa-2x"></i></a></li>  
+                        <li><a class="facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://www.google.com/" target="_blank"><i className="fab fa-facebook fa-2x"></i></a></li>
+                        <li><a class="twitter" href="https://www.twitter.com/share?url=https://www.google.com/" target="_blank"><i className="fab fa-twitter fa-2x"></i></a></li>
+                        <li><a class="reddit" href="http://www.reddit.com/submit?url=https://www.google.com/" target="_blank"><i className="fab fa-reddit-alien fa-2x"></i></a></li>
+                        <li><a class="linkedin" href="https://www.linkedin.com/sharing/share-offsite/?url=https://www.google.com/" target="_blank"><i className="fab fa-linkedin fa-2x"></i></a></li>  
                     </ul>
                 </div>
             </footer>
